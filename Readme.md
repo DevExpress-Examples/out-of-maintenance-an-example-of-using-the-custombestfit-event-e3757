f24134ea7be7df0e391d93956c9fbc775e2afe9a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# An example of using the CustomBestFit event
+
+
+<p>This example shows how to specify rows that should be taken into account when calculating the optimal width of a column. Specify required rows by selecting their handles in the list box, invoke the Best Fit method by right-clicking the column's header and choosing the correspondent menu item.</p>
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace WpfApplication5
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			Dim source() As String = { "123456789", "1234567890", "12345678901", "123456789012" }
+			gridControl1.ItemsSource = source
+
+		End Sub
+		Private Sub tableView1_CustomBestFit(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomBestFitEventArgs)
+			Dim selectedRows As New LinkedList(Of Integer)()
+			For Each item As ListBoxItem In listBoxEdit1.SelectedItems
+				If item.IsEnabled Then
+					selectedRows.AddLast(Convert.ToInt32(item.Content))
+				End If
+			Next item
+			e.BestFitRows = selectedRows
+		End Sub
+	End Class
+End Namespace
