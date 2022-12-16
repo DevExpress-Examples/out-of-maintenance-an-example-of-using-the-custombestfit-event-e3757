7d656aa6841d34fa29b81408899c93ba5b6f26bb
DevExpressExampleBot <fakeEmail>
2022-12-16 15:38:31 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,29 +1,29 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace WpfApplication5
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			Dim source() As String = { "123456789", "1234567890", "12345678901", "123456789012" }
-			gridControl1.ItemsSource = source
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		End Sub
-		Private Sub tableView1_CustomBestFit(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomBestFitEventArgs)
-			Dim selectedRows As New LinkedList(Of Integer)()
-			For Each item As ListBoxItem In listBoxEdit1.SelectedItems
-				If item.IsEnabled Then
-					selectedRows.AddLast(Convert.ToInt32(item.Content))
-				End If
-			Next item
-			e.BestFitRows = selectedRows
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim source As String() = New String() {"123456789", "1234567890", "12345678901", "123456789012"}
+            Me.gridControl1.ItemsSource = source
+        End Sub
+
+        Private Sub tableView1_CustomBestFit(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomBestFitEventArgs)
+            Dim selectedRows As LinkedList(Of Integer) = New LinkedList(Of Integer)()
+            For Each item As ListBoxItem In Me.listBoxEdit1.SelectedItems
+                If item.IsEnabled Then selectedRows.AddLast(Convert.ToInt32(item.Content))
+            Next
+
+            e.BestFitRows = selectedRows
+        End Sub
+    End Class
 End Namespace
